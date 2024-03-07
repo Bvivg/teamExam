de324f1819ab9dad2c35093f9b3b7268145175b3
Bvivg <tatytygulovbatyrbek19@gmail.com>
2024-03-07 10:56:43 +0600

    components

--- src/components/Layout/Header.jsx
+++ src/components/Layout/Header.jsx
@@ -1,11 +1,25 @@
-import React from 'react'
-
+import React from "react";
+import NavLink from "@/UI/NavLink/NavLink";
+import { ChatIcon, SettingsIcon } from "@/icons/Icons";
 const Header = () => {
   return (
-    <div className='header'>
-      
+    <div className="header">
+      <a href="/" className="logo">
+        <img src="@/public/LogoLight.png" alt="" />
+      </a>
+      <nav className="nav">
+        <NavLink to="support">
+          <ChatIcon /> Поддержка
+        </NavLink>
+        <NavLink to="orders">
+          <PhoneIcon /> Мои заказы
+        </NavLink>
+        <NavLink to="settings">
+          <SettingsIcon/> Настройки
+        </NavLink>
+      </nav>
     </div>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
--- src/components/UI/NavLink/NavLink.jsx
+++ src/components/UI/NavLink/NavLink.jsx
@@ -1,9 +1,11 @@
 import React from "react";
 import Button from "../Button/Button";
+import { redirect } from "next/navigation";
 
 const NavLink = ({ content, to }) => {
+  const onNavigate = () => redirect(to)
   return (
-    <Button theme="navLink" event={to}>
+    <Button theme="navLink" event={onNavigate}>
       {content}
     </Button>
   );
