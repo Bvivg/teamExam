8daca73a4c782c26b8fe7e289ee6ca2b04bd3c72
Bvivg <tatytygulovbatyrbek19@gmail.com>
2024-03-14 11:33:15 +0600

    header & footer

--- src/components/Layout/Footer.jsx
+++ src/components/Layout/Footer.jsx
@@ -1,8 +1,8 @@
-"use client";
 import React from "react";
 import styles from "./style.module.scss";
 import Button from "../UI/Button/Button";
 import {
+  FooterBg,
   MasterCard,
   SupportIcon,
   VisaIcon,
@@ -10,36 +10,55 @@ import {
   YandexIcon,
 } from "@/icons/Icons";
 import NavLink from "../UI/NavLink/NavLink";
-const Footer = () => {
-  return (
-    <div className={styles.container}>
-      <div className={styles.footer}>
-        <div className={styles.payment}>
-          <span>Способы оплаты</span>
-          <Button primary="tertiary" size="xm">
-            <VisaIcon />
-          </Button>
-          <Button primary="tertiary" size="xm">
-            <MasterCard />
-          </Button>
-          <Button primary="tertiary" size="xm">
-            <YandexIcon />
-          </Button>
-          <Button primary="tertiary" size="xm">
-            <WebMoneyIcon />
-          </Button>
-        </div>
-        <span className={styles.rights}>Права защищены и опасны</span>
-        <div className={styles.footerAction}>
-          <NavLink to="/support">
-            <SupportIcon /> Поддержка
-          </NavLink>
+import clsx from "clsx";
 
-          <Button primary="primary" size="xl">
-            Найти заказ
-          </Button>
+const Footer = ({ theme }) => {
+  return (
+    <div
+      className={clsx(
+        styles.container,
+        theme === "light"
+          ? styles.container_footer_light
+          : theme === "cursed"
+          ? styles.footer_none
+          : ""
+      )}
+      style={{ marginTop: "auto" }}
+    >
+      {theme === "light" ? (
+        <div className={styles.container_footer_light_bg}>
+          <FooterBg />
         </div>
-      </div>
+      ) : null}
+      {theme === "default" || theme === "light" ? (
+        <div className={styles.footer}>
+          <div className={styles.payment}>
+            <span>Способы оплаты</span>
+            <Button primary="tertiary" size="xm">
+              <VisaIcon />
+            </Button>
+            <Button primary="tertiary" size="xm">
+              <MasterCard />
+            </Button>
+            <Button primary="tertiary" size="xm">
+              <YandexIcon />
+            </Button>
+            <Button primary="tertiary" size="xm">
+              <WebMoneyIcon />
+            </Button>
+          </div>
+          <span className={styles.rights}>Права защищены и опасны</span>
+          <div className={styles.footerAction}>
+            <NavLink to="/support">
+              <SupportIcon /> Поддержка
+            </NavLink>
+
+            <Button primary="primary" size="xl">
+              Найти заказ
+            </Button>
+          </div>
+        </div>
+      ) : null}
     </div>
   );
 };
--- src/components/Layout/Header.jsx
+++ src/components/Layout/Header.jsx
@@ -1,6 +1,15 @@
 "use client";
 import { useState } from "react";
-import { ChatIcon, SettingsIcon, PhoneIcon, MoneyIcon } from "@/icons/Icons";
+import {
+  ChatIcon,
+  SettingsIcon,
+  PhoneIcon,
+  MoneyIcon,
+  LogoLight,
+  LogoDark,
+  SupportIcon,
+  UserIcon,
+} from "@/icons/Icons";
 import NavLink from "../UI/NavLink/NavLink";
 import styles from "./style.module.scss";
 import Button from "../UI/Button/Button";
@@ -8,66 +17,99 @@ import Avatar from "../UI/Avatar/Avatar";
 import Link from "next/link";
 import clsx from "clsx";
 
-const Header = () => {
+const Header = ({ theme }) => {
   const [isActive, setIsActive] = useState(false);
+
   return (
-    <div className={styles.container}>
+    <div
+      className={
+        theme === "default" || theme === "cursed"
+          ? styles.container
+          : styles.container_light
+      }
+    >
       <div className={styles.header}>
         <Link href="/" className={styles.logo}>
-          <img src="/LogoLight.png" alt="logo" />
+          {theme === "default" || theme === "cursed" ? (
+            <LogoLight />
+          ) : theme === "light" ? (
+            <LogoDark />
+          ) : (
+            ""
+          )}
         </Link>
-        <div
-          className={clsx(styles.hamburger, isActive ? styles.active : "")}
-          onClick={() => {
-            setIsActive(!isActive);
-          }}
-        >
-          <span></span>
-          <span></span>
-          <span></span>
-        </div>
-        <div
-          className={clsx(styles.nav_collapse, isActive ? styles.active : "")}
-        >
-          <div className={styles.balance}>
-            <div className={styles.balanceInfo}>
-              <span>
-                <MoneyIcon /> Баланс:
-              </span>
-              <Button primary="primary" size="xm">
-                1234 ₽
+        {theme === "default" ? (
+          <div
+            className={clsx(styles.hamburger, isActive ? styles.active : "")}
+            onClick={() => {
+              setIsActive(!isActive);
+            }}
+          >
+            <span></span>
+            <span></span>
+            <span></span>
+          </div>
+        ) : theme === "light" ? (
+          <div className={clsx(styles.nav_collapse, styles.nav_collapse_light)}>
+            <nav className={clsx(styles.nav, styles.nav_light)}>
+              <Link href="/about">О сервисе</Link>
+              <NavLink to="/support">
+                <SupportIcon /> Поддержка
+              </NavLink>
+              <Button primary="primary" size="xl">
+                Войти <UserIcon />
               </Button>
+            </nav>
+          </div>
+        ) : (
+          ""
+        )}
+        {theme === "default" ? (
+          <div
+            className={clsx(styles.nav_collapse, isActive ? styles.active : "")}
+          >
+            <div className={styles.balance}>
+              <div className={styles.balanceInfo}>
+                <span>
+                  <MoneyIcon /> Баланс:
+                </span>
+                <Button primary="primary" size="xm">
+                  1234 ₽
+                </Button>
+              </div>
+              <div className={styles.balanceAction}>
+                <Button primary="tertiary" size="xm">
+                  Пополнить
+                </Button>
+                <Button primary="tertiary" size="xm">
+                  Вывести
+                </Button>
+              </div>
             </div>
-            <div className={styles.balanceAction}>
-              <Button primary="tertiary" size="xm">
-                Пополнить
+            <nav className={styles.nav}>
+              <NavLink to="/support">
+                <ChatIcon /> Поддержка
+              </NavLink>
+              <NavLink to="/orders">
+                <PhoneIcon /> Мои заказы
+              </NavLink>
+              <NavLink to="/settings">
+                <SettingsIcon /> Настройки
+              </NavLink>
+            </nav>
+            <div className={styles.profile}>
+              <Button primary="primary" size="xl">
+                Разместить заказ
               </Button>
-              <Button primary="tertiary" size="xm">
-                Вывести
+              <Avatar imgPath="/LogoLight.png" imgSize="xm"></Avatar>
+              <Button primary="tertiary" size="xl">
+                Выйти
               </Button>
             </div>
           </div>
-          <nav className={styles.nav}>
-            <NavLink to="/support">
-              <ChatIcon /> Поддержка
-            </NavLink>
-            <NavLink to="/orders">
-              <PhoneIcon /> Мои заказы
-            </NavLink>
-            <NavLink to="/settings">
-              <SettingsIcon /> Настройки
-            </NavLink>
-          </nav>
-          <div className={styles.profile}>
-            <Button primary="primary" size="xl">
-              Разместить заказ
-            </Button>
-            <Avatar imgPath="/LogoLight.png" imgSize="xm"></Avatar>
-            <Button primary="tertiary" size="xl">
-              Выйти
-            </Button>
-          </div>
-        </div>
+        ) : (
+          ""
+        )}
       </div>
     </div>
   );
